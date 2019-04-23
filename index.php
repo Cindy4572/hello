be06f898266a077ec05af9fbc00c81cd79049852
Cindy LOISON <cindyloison@hotmail.fr>
2019-04-23 12:12:41 +0200

    ajout d'une nouvelle annonce

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
+
 require "function.php";
 $resultat = getAllAnnonces();
+
 ?>
+
 <ul>
     <li><?php echo $resultat["annonce1"][0] ?></li>
     <li><?php echo $resultat["annonce1"][1] ?></li>
 </ul>
+
+<hr>
+
+<ul>
+    <li><?php echo $resultat["annonce2"][0] ?></li>
+</ul>
+
